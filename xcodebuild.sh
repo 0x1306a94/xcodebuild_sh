befc4ec2f8bd574233572f062300ff6948e57652
sunsong <sunsong@taihe.com>
2016-11-28 16:34:27 +0800

    change script

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -3,11 +3,35 @@
 echo "~~~~~~~~~~~~~~~~开始执行脚本~~~~~~~~~~~~~~~~"
 
 
+####################################################################
+###################工程信息以及最下面的蒲公英信息########################
+#工程名
+PROJECTNAME="xxxx"
+#需要编译的 targetName
+TARGET_NAME="xxxx"
+# ADHOC
+#证书名#描述文件
+ADHOCCODE_SIGN_IDENTITY="xxxxx"
+ADHOCPROVISIONING_PROFILE_NAME="xxxx"
+
+#AppStore证书名#描述文件
+APPSTORECODE_SIGN_IDENTITY="xxxxx"
+APPSTOREADHOCPROVISIONING_PROFILE_NAME="xxxx"
+
+#是否是工作空间
+ISWORKSPACE=true
+####################################################################
+
+#证书名
+CODE_SIGN_IDENTITY=${ADHOCCODE_SIGN_IDENTITY}
+#描述文件
+PROVISIONING_PROFILE_NAME=${ADHOCPROVISIONING_PROFILE_NAME}
+
+
 # 开始时间
 beginTime=`date +%s`
 DATE=`date '+%Y-%m-%d-%T'`
-#需要编译的 targetName
-TARGET_NAME="xxxx"
+
 #编译模式 工程默认有 Debug Release 
 CONFIGURATION_TARGET=Release
 #编译路径
@@ -17,14 +41,6 @@ ARCHIVEPATH=${BUILDPATH}/${TARGET_NAME}.xcarchive
 #输出的ipa目录
 IPAPATH=${BUILDPATH}
 
-#证书名
-CODE_SIGN_IDENTITY="xxxxx"
-#描述文件
-PROVISIONING_PROFILE_NAME="xxxx"
-
-#苹果账号
-AppleID="xxxx"
-AppleIDPWD="xxxx"
 
 #导出ipa 所需plist
 ADHOCExportOptionsPlist=./ADHOCExportOptionsPlist.plist
@@ -35,8 +51,6 @@ ExportOptionsPlist=${ADHOCExportOptionsPlist}
 
 # 是否上传蒲公英
 UPLOADPGYER=false
-# 是否上传AppStore
-UPLOADAPPSTore=false
 
 echo "~~~~~~~~~~~~~~~~选择打包方式~~~~~~~~~~~~~~~~"
 echo "		1 ad-hoc (默认)"
@@ -52,12 +66,13 @@ if [ -n "$method" ]
 then
 	if [ "$method" = "1" ]
 	then 
-	PROVISIONING_PROFILE_NAME="xxxx"
+	CODE_SIGN_IDENTITY=${ADHOCCODE_SIGN_IDENTITY}
+    PROVISIONING_PROFILE_NAME=${ADHOCPROVISIONING_PROFILE_NAME}
 	ExportOptionsPlist=${ADHOCExportOptionsPlist}
 	elif [ "$method" = "2" ]
 	then
-	UPLOADAPPSTore=true
-	PROVISIONING_PROFILE_NAME="xxxx"
+	CODE_SIGN_IDENTITY=${APPSTORECODE_SIGN_IDENTITY}
+    PROVISIONING_PROFILE_NAME=${APPSTOREADHOCPROVISIONING_PROFILE_NAME}
 	ExportOptionsPlist=${AppStoreExportOptionsPlist}
 	else
 	echo "参数无效...."
@@ -67,47 +82,60 @@ else
 	ExportOptionsPlist=${ADHOCExportOptionsPlist}
 fi
 
-if [ $UPLOADAPPSTore = false ]
-then
-	echo "~~~~~~~~~~~~~~~~是否上传蒲公英~~~~~~~~~~~~~~~~"
-	echo "		1 不上传 (默认)"
-	echo "		2 上传 "
-	read para
-	sleep 0.5
+echo "~~~~~~~~~~~~~~~~是否上传蒲公英~~~~~~~~~~~~~~~~"
+echo "		1 不上传 (默认)"
+echo "		2 上传 "
+read para
+sleep 0.5
 
-	if [ -n "$para" ]
+if [ -n "$para" ]
+then
+	if [ "$para" = "1" ]
+	then 
+	UPLOADPGYER=false
+	elif [ "$para" = "2" ]
 	then
-		if [ "$para" = "1" ]
-		then 
-		UPLOADPGYER=false
-		elif [ "$para" = "2" ]
-		then
-		UPLOADPGYER=true
-		else
-		echo "参数无效...."
-		exit 1
-		fi
+	UPLOADPGYER=true
 	else
-		UPLOADPGYER=false
+	echo "参数无效...."
+	exit 1
 	fi
+else
+	UPLOADPGYER=false
 fi
 
 
 echo "~~~~~~~~~~~~~~~~开始编译~~~~~~~~~~~~~~~~~~~"
-echo "~~~~~~~~~~~~~~~~开始清理~~~~~~~~~~~~~~~~~~~"
+
+
+if [ $ISWORKSPACE = true ]
+then
 # 清理 避免出现一些莫名的错误
-xcodebuild clean -workspace ${TARGET_NAME}.xcworkspace \
+xcodebuild clean -workspace ${PROJECTNAME}.xcworkspace \
 -configuration \
 ${CONFIGURATION} -alltargets
 
-echo "~~~~~~~~~~~~~~~~开始构建~~~~~~~~~~~~~~~~~~~"
 #开始构建
-xcodebuild archive -workspace ${TARGET_NAME}.xcworkspace \
+xcodebuild archive -workspace ${PROJECTNAME}.xcworkspace \
 -scheme ${TARGET_NAME} \
 -archivePath ${ARCHIVEPATH} \
 -configuration ${CONFIGURATION_TARGET} \
 CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
 PROVISIONING_PROFILE="${PROVISIONING_PROFILE_NAME}"
+else
+# 清理 避免出现一些莫名的错误
+xcodebuild clean -xcodeproj ${PROJECTNAME}.xcodeproj \
+-configuration \
+${CONFIGURATION} -alltargets
+
+#开始构建
+xcodebuild archive -xcodeproj ${PROJECTNAME}.xcodeproj \
+-scheme ${TARGET_NAME} \
+-archivePath ${ARCHIVEPATH} \
+-configuration ${CONFIGURATION_TARGET} \
+CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+PROVISIONING_PROFILE="${PROVISIONING_PROFILE_NAME}"
+fi
 
 echo "~~~~~~~~~~~~~~~~检查是否构建成功~~~~~~~~~~~~~~~~~~~"
 # xcarchive 实际是一个文件夹不是一个文件所以使用 -d 判断
@@ -137,6 +165,7 @@ IPAPATH=${IPAPATH}/${TARGET_NAME}.ipa
 if [ -f "$IPAPATH" ]
 then
 echo "导出ipa成功......"
+open $BUILDPATH
 else
 echo "导出ipa失败......"
 # 结束时间
@@ -149,56 +178,24 @@ fi
 endTime=`date +%s`
 ExportTime="导出ipa时间$[ endTime - beginTime ]秒"
 
-# 上传AppStore
-if [ $UPLOADAPPSTore = true ]
-then	
+# 上传蒲公英	
+if [ $UPLOADPGYER = true ]
+then
+	echo "~~~~~~~~~~~~~~~~上传ipa到蒲公英~~~~~~~~~~~~~~~~~~~"
+	curl -F "file=@$IPAPATH" \
+	-F "uKey=xxxxx" \
+	-F "_api_key=xxxx" \
+	-F "password=xxxxx" \
+	-F "isPublishToPublic=xxxx" \
+	https://www.pgyer.com/apiv1/app/upload --verbose
 
-	altoolPath="/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
-	${altoolPath} --validate-app \
-	-f ${IPAPATH} \
-	-u ${AppleID} \
-	-p ${AppleIDPWD} \
-	-t ios --output-format xml
-
-		if [ $? = 0 ]
-		then
-		echo "~~~~~~~~~~~~~~~~验证ipa成功~~~~~~~~~~~~~~~~~~~"
-			${altoolPath} --upload-app \
-			-f ${IPAPATH} \
-			-u ${AppleID} \
-			-p ${AppleIDPWD} \
-			-t ios --output-format xml
-
-			if [ $? = 0 ]
-			then
-			echo "~~~~~~~~~~~~~~~~提交AppStore成功~~~~~~~~~~~~~~~~~~~"
-			else
-			echo "~~~~~~~~~~~~~~~~提交AppStore失败~~~~~~~~~~~~~~~~~~~"
-			fi
-		else
-		echo "~~~~~~~~~~~~~~~~验证ipa失败~~~~~~~~~~~~~~~~~~~"
-		fi
-else
-	# 上传蒲公英	
-	if [ $UPLOADPGYER = true ]
+	if [ $? = 0 ]
 	then
-		echo "~~~~~~~~~~~~~~~~上传ipa到蒲公英~~~~~~~~~~~~~~~~~~~"
-		curl -F "file=@$IPAPATH" \
-		-F "uKey=xxxxx" \
-		-F "_api_key=xxxx" \
-		-F "password=xxxxx" \
-		-F "isPublishToPublic=xxxx" \
-		https://www.pgyer.com/apiv1/app/upload --verbose
-
-		if [ $? = 0 ]
-		then
-		echo "~~~~~~~~~~~~~~~~上传蒲公英成功~~~~~~~~~~~~~~~~~~~"
-		else
-		echo "~~~~~~~~~~~~~~~~上传蒲公英失败~~~~~~~~~~~~~~~~~~~"
-		fi
+	echo "~~~~~~~~~~~~~~~~上传蒲公英成功~~~~~~~~~~~~~~~~~~~"
+	else
+	echo "~~~~~~~~~~~~~~~~上传蒲公英失败~~~~~~~~~~~~~~~~~~~"
 	fi
 fi
-
 
 
 echo "~~~~~~~~~~~~~~~~配置信息~~~~~~~~~~~~~~~~~~~"
